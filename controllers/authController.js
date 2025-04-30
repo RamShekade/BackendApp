ec74cfb09d659215c14ec1c23cb9247d023b1221
RamShekade <127588613+RamShekade@users.noreply.github.com>
2025-04-30 23:36:27 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 // controllers/authController.js
-const User = require('../models/user');
+const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const PersonalInfo = require('../models/personalInfoSchema');
